bc811db732cbcdcf4ee5bf4ff7c91876a06434ab
Ryan <ryanfelipe.rr@gmail.com>
2024-02-13 06:55:00 -0300

    Initial commit

---
+++ src/main/java/com/org/artpapel/ecommerce/ArtePapelApplication.java
@@ -0,0 +1,13 @@
+package com.org.artpapel.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtePapelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArtePapelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/org/artpapel/ecommerce/ArtePapelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.org.artpapel.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtePapelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
